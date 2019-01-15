3dc4d488f5da21dff8657aeb586f95cf1c7c72ed
ymh <ymh@DESKTOP-JF2UR19>
2019-01-15 13:59:39 +0900

    add line

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hi World");
 		System.out.println("HelloWorld");
+		System.out.println("HelloMaMa");
 	}
 }
